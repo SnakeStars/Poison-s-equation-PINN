70e620e2c26b8261e0add591cc5af022375dae0b
Ivan <solovev074@mail.ru>
2024-09-12 21:26:45 +0300

    stoped on loss func

--- Neural_Network.py
+++ Neural_Network.py
@@ -2,7 +2,9 @@ import os
 import torch
 from torch import nn
 from torch.utils.data import DataLoader
+from torch.utils.tensorboard import SummaryWriter
 from torchvision import datasets, transforms
+from tqdm import tqdm
 
 # Выбор ресурса для обучения (автоматический)
 
@@ -41,6 +43,8 @@ class NeuralNetwork(nn.Module):
 model = NeuralNetwork().to(device)
 print(model)
 
+# Уравнение функции
+
 def pde(outx, outy, fx, fy):
 
     dudx = (torch.autograd.grad(outx, fx, torch.ones_like(fx), create_graph=True,
@@ -54,14 +58,78 @@ def pde(outx, outy, fx, fy):
                             retain_graph=True)[0])
     return d2udx2 + d2udy2
 
+# Уравнение ошибки
+
+def pdeLoss(t):
+    out = model(t).to(device)
+    f1 = pde(out, t)
+
+    inlet_mask = (t[:, 0] == 0)
+    t0 = t[inlet_mask]
+    x0 = model(t0).to(device)
+    dx0dt = torch.autograd.grad(x0, t0, torch.ones_like(t0), create_graph=True, \
+                        retain_graph=True)[0]
+
+    loss_bc = metric_data(x0, x0_true) + \
+                metric_data(dx0dt, dx0dt_true.to(device))
+    loss_pde = metric_data(f1, torch.zeros_like(f1))
+
+    loss = 1e3*loss_bc + loss_pde
+
+    metric_x = metric_data(out, x0_true * torch.sin(omega*t + torch.pi / 2))
+    metric_x0 = metric_data(x0, x0_true)
+    metric_dx0dt = metric_data(dx0dt, dx0dt_true.to(device))
+
+    acc_metrics = {'metric_x': metric_x,
+                'metric_x0': metric_x0,
+                'metric_dx0dt': metric_dx0dt,
+                }
+
+    metrics = {'loss': loss,
+                'loss_bc': loss_bc,
+                'loss_pde': loss_pde,
+                }
+    for k, v in metrics.items():
+        run[k].append(v)
+    for k, v in acc_metrics.items():
+        run[k].append(v)
+
+    return loss
+
+
 
 if __name__ == "__main__":
 
     #  Задание параметров модели:
+
     Q = [[0, 2], [0, 2]]                    # Borders
     step = 10                               # points in one dim
+    EPOH = 1000                             # study iterations
+
+    def f(x, y):
+        return (-2) * (torch.pi ** 2) * torch.sin(torch.pi * x) * torch.sin(torch.pi * y)
+    
+
+    def g(x,y):
+        return torch.sin(torch.pi * x) * torch.sin(torch.pi * y)
+    
+    def F(x,y, Q, res):
+        return g(x,y) + (x - Q[0][0])*(Q[0][1] - x)*(y - Q[1][0])*(Q[1][1] - y) * res
 
 
     # Создание сетки:
+
     t = torch.cartesian_prod(torch.linspace(Q[0][0], Q[0][1], step), torch.linspace(Q[1][0], Q[1][1], step))
-    print(t)
+    t.requires_grad(True)
+
+
+    # Создание шкалы загрузки:
+
+    pbar = tqdm(range(EPOH), desc='Training Progress')
+
+
+    # Оптимизатор и функция подсчета ошибки
+
+    metric_data = nn.MSELoss()
+    writer = SummaryWriter()
+    optimizer = torch.optim.LBFGS(model.parameters(), lr=0.1)
